41802ed7188949c804f171e796ac6a1a5af0c1a7
daniesquivelc97 <dani_esquivelc@hotmail.com>
2021-06-28 14:59:57 -0500

    Aplicación de preferencias de usuario

---
+++ lib/src/share_prefs/preferencias_usuario.dart
@@ -0,0 +1,51 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
+class PreferenciasUsuario {
+
+  static final PreferenciasUsuario _instancia = new PreferenciasUsuario._internal();
+  factory PreferenciasUsuario() {
+    return _instancia;
+  }
+  PreferenciasUsuario._internal();
+  SharedPreferences _prefs;
+  
+  initPrefs() async {
+    this._prefs = await SharedPreferences.getInstance();
+  }
+
+  // Getters y setters de genero
+  get genero {
+    return _prefs.getInt('genero') ?? 1;
+  }
+
+  set genero(int value) {
+    _prefs.setInt('genero', value);
+  }
+
+  // Getters y setters de color secundario
+  get colorSecundario {
+    return _prefs.getBool('colorSecundario') ?? false;
+  }
+
+  set colorSecundario(bool value) {
+    _prefs.setBool('colorSecundario', value);
+  }
+
+  // Getters y setters de nombre usuario
+  get nombreUsuario {
+    return _prefs.getString('nombreUsuario') ?? '';
+  }
+
+  set nombreUsuario(String value) {
+    _prefs.setString('nombreUsuario', value);
+  }
+
+  // Getters y setters de ultima pagina
+  get ultimaPagina {
+    return _prefs.getString('ultimaPagina') ?? 'home';
+  }
+
+  set ultimaPagina(String value) {
+    _prefs.setString('ultimaPagina', value);
+  }
+}
